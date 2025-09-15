e7f5784092d655cd0b4d9ef3c39a4ae3a3db8077
taek-99 <gunha1215@gmail.com>
2025-09-15 23:32:36 +0900

    tt

---
+++ day2/main.js
@@ -0,0 +1,25 @@
+// 로그인 구현
+
+/*
+let admin = 'taek';
+let adminword = '1234';
+
+let username = prompt('아이디 입력하시오');
+
+if (username == admin) {
+    let usernpassword = prompt('비번 입력하시오');
+
+    if (usernpassword == adminword) {
+        alert("환영한다")
+    }
+}
+*/
+
+
+for (let i = 1; i <= 9; i++) {
+    for (let j = 1; j < 9; j++) {
+        let aa = i * j
+        alert(aa)
+    }
+
+}
