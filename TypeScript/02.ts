b53ef34763acdfb9d9dd2cbab8fb6689e9415736
taek-99 <gunha1215@gmail.com>
2025-10-21 13:55:46 +0900

    tt

---
+++ TypeScript/02.ts
@@ -0,0 +1,18 @@
+
+// let a : number[] = [1, 2];
+
+
+// type Age = number;
+
+// type player = {
+//     readonly name: string,
+//     age?: Age
+// }
+
+
+// const playerMager = (name: string): player => ({name})
+// const nico = playerMager("nico")
+
+
+const player: [string, number, boolean] = ["tt", 1, true]
+
