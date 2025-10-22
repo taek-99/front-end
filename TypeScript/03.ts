8a0fbc90970508baba8f70f06fe1f89e3a7e9052
taek-99 <gunha1215@gmail.com>
2025-10-22 15:13:11 +0900

    tt

---
+++ TypeScript/03.ts
@@ -0,0 +1,24 @@
+
+
+
+// type ADD = {
+//     (a: number, b:number) : number
+// }
+
+// const add:ADD = (a, b) => {
+//     return (
+//         a+b
+//     );
+// }
+
+
+
+type Add = {
+    (a: number, b:number) : number,
+    (a: number, b: number, c: number) : number,
+}
+
+const add:Add = (a, b, c?) => {
+    return a + b
+}
+
